b709a319522ae1658b0f5dc4eb72e6e8820f8286
Alla Ponurko <ponurkoalla264@gmail.com>
2022-01-27 11:28:14 +0300

    Добавьте файлы проекта.

---
+++ InternetStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InternetStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
